7c217b0342e27b4849ebe8b38e5d2c88ca9b43e7
GDJ66 <jhl1086@naver.com>
2023-06-01 11:41:54 +0900

    Customer/Employees/Orders/Product vo 추가

---
+++ lightingShop/src/main/java/vo/Product.java
@@ -0,0 +1,89 @@
+package vo;
+
+public class Product {
+	private int productNo;
+	private String categoryName;
+	private String productName;
+	private double productPrice;
+	private String productStatus;
+	private String productStock;
+	private String productInfo;
+	private int cstmPoint;
+	private String cstmLastLogin;
+	private String cstmAgree;
+	private String createdate;
+	private String updatedate;
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	public String getProductStatus() {
+		return productStatus;
+	}
+	public void setProductStatus(String productStatus) {
+		this.productStatus = productStatus;
+	}
+	public String getProductStock() {
+		return productStock;
+	}
+	public void setProductStock(String productStock) {
+		this.productStock = productStock;
+	}
+	public String getProductInfo() {
+		return productInfo;
+	}
+	public void setProductInfo(String productInfo) {
+		this.productInfo = productInfo;
+	}
+	public int getCstmPoint() {
+		return cstmPoint;
+	}
+	public void setCstmPoint(int cstmPoint) {
+		this.cstmPoint = cstmPoint;
+	}
+	public String getCstmLastLogin() {
+		return cstmLastLogin;
+	}
+	public void setCstmLastLogin(String cstmLastLogin) {
+		this.cstmLastLogin = cstmLastLogin;
+	}
+	public String getCstmAgree() {
+		return cstmAgree;
+	}
+	public void setCstmAgree(String cstmAgree) {
+		this.cstmAgree = cstmAgree;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	
+}
