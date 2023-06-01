577061e0ce866b3a7f249020b334081f74e491f2
Kim-Younghun <floriaberg59@gmail.com>
2023-06-01 14:49:25 +0900

    [김영훈] IdList/PointHistory/PwHistory vo 추가

---
+++ lightingShop/src/main/java/vo/IdList.java
@@ -0,0 +1,33 @@
+package vo;
+
+public class IdList {
+	private String id;
+	private String lastPw;
+	private String active;
+	private String createdate;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLastPw() {
+		return lastPw;
+	}
+	public void setLastPw(String lastPw) {
+		this.lastPw = lastPw;
+	}
+	public String getActive() {
+		return active;
+	}
+	public void setActive(String active) {
+		this.active = active;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+}
