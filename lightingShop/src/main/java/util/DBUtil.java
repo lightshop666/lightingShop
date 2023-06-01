3e550acec8657a166a02e5771c8b45fd215a0dc1
huijin0809 <huijin0809@naver.com>
2023-06-01 13:55:42 +0900

    DB연결 코드

---
+++ lightingShop/src/main/java/util/DBUtil.java
@@ -0,0 +1,16 @@
+package util;
+import java.sql.*;
+
+public class DBUtil {
+	public Connection getConnection() throws Exception {
+		String driver = "org.mariadb.jdbc.Driver";
+		String dburl = "jdbc:mariadb://127.0.0.1:3306/shop";
+		String dbuser = "root";
+		String dbpw = "java1234";
+		
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(dburl, dbuser, dbpw);
+		
+		return conn;
+	}
+}
